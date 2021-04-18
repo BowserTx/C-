d89024e61de2417df38d8f2f996b2ec7a008dc98
Willian Souza <76187694+BowserTx@users.noreply.github.com>
2021-04-17 20:40:39 -0400

    Code Wars

---
+++ Will you make it.cs
@@ -0,0 +1,16 @@
+using System;
+
+public static class Kata
+{
+  public static bool ZeroFuel(uint distanceToPump, uint mpg, uint fuelLeft)
+  {
+      if(mpg*fuelLeft >= distanceToPump)
+        {
+          return true;
+        }
+      else
+        {
+          return false;
+        }
+  }
+}
