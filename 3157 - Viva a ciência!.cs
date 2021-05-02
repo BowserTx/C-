b124ba87699cafbd386dbbc8a9cfa510e50c2595
Willian Souza <76187694+BowserTx@users.noreply.github.com>
2021-05-02 19:27:45 -0400

    Uri code

---
+++ 3157 - Viva a ciência!.cs
@@ -0,0 +1,18 @@
+using System;
+
+
+namespace Main
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string ciencia = Console.ReadLine();
+
+            Console.WriteLine("Nao se refuta ciencia com opiniao");
+
+        }
+    }
+}
+
